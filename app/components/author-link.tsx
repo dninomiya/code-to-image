e065f7f0513d2abf4b45adc9ceba65fb107978d0
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-12-27 21:03:07 +0900

    fix author link style

--- app/components/author-link.tsx
+++ app/components/author-link.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AuthorLink() {
   return (
-    <div className="p-3 rounded-xl border flex items-center gap-3 fixed bottom-4 right-4 bg-zinc-900 text-zinc-500 border-zinc-700">
+    <div className="p-3 rounded-xl border flex items-center gap-3 fixed bottom-4 right-4 bg-muted/50 text-muted-foreground">
       <img
         src="https://pbs.twimg.com/profile_images/1663723879809032192/86vmGeJL_400x400.png"
         className="w-8 h-8 rounded-full"
