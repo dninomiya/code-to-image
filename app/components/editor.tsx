1ce8f056f81adf949320c28d44afca8558ac0adc
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2023-12-27 20:58:23 +0900

    add hot key

--- app/components/editor.tsx
+++ app/components/editor.tsx
@@ -1,13 +1,42 @@
 'use client';
 
 import { useApp } from '@/app/providers/app-provider';
-import { useEffect, useState } from 'react';
+import { KeyboardEventHandler, useCallback, useEffect, useState } from 'react';
 import { codeToHtml } from 'shikiji/bundle/web';
 
 export default function Editor() {
   const [value, setValue] = useState<string>('');
   const [html, setHTML] = useState<string>('');
   const { lang } = useApp();
+  const handleHotKey: KeyboardEventHandler<HTMLTextAreaElement> = useCallback(
+    (e) => {
+      const shiftKey = e.shiftKey;
+      const textarea = e.target as HTMLTextAreaElement;
+      const val = textarea.value;
+      const start = textarea.selectionStart;
+      const end = textarea.selectionEnd;
+      const tabSize = 2;
+
+      if (e.key === 'Tab') {
+        e.preventDefault();
+
+        if (shiftKey) {
+          if (val.substring(start - tabSize, start) === ' '.repeat(tabSize)) {
+            textarea.value =
+              val.substring(0, start - tabSize) + val.substring(start);
+            textarea.selectionStart = textarea.selectionEnd = start - tabSize;
+          }
+        } else {
+          textarea.value =
+            val.substring(0, start) + ' '.repeat(tabSize) + val.substring(end);
+          textarea.selectionStart = textarea.selectionEnd = start + tabSize;
+        }
+
+        textarea.dispatchEvent(new Event('input'));
+      }
+    },
+    []
+  );
 
   useEffect(() => {
     codeToHtml(value, {
@@ -41,6 +70,7 @@ export default function Editor() {
     <div className="relative">
       <textarea
         value={value}
+        onKeyDown={handleHotKey}
         onChange={(e) => {
           resize(e.target);
           setValue(e.target.value);
